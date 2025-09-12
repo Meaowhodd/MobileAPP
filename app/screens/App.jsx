4702610acf2afe7a410de12efa9d654d3dc17964
Starlipp04 <cokerter159630@gmail.com>
2025-09-12 21:35:24 +0700

    Update App.jsx

--- app/screens/App.jsx
+++ app/screens/App.jsx
@@ -1,20 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import LoginScreen from "./screens/LoginScreen"; // ปรับ path ให้ตรงกับไฟล์ของคุณ
-import Tabs from "./Tab"; // ใช้โค้ดเพื่อน
-
-const Stack = createNativeStackNavigator();
+import Tabs from "./Tab"; // ปรับ path ตามจริง
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* หน้าแรก = Login */}
-        <Stack.Screen name="Login" component={LoginScreen} />
-        {/* ถ้า login สำเร็จ จะเข้า Tabs */}
-        <Stack.Screen name="Main" component={Tabs} />
-      </Stack.Navigator>
+      <Tabs />
     </NavigationContainer>
   );
 }
