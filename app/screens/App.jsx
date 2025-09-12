ce01731352e2d86a8f3b9d46854e3899674d0126
Starlipp04 <cokerter159630@gmail.com>
2025-09-12 22:22:13 +0700

    Update App.jsx

--- app/screens/App.jsx
+++ app/screens/App.jsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./Tab"; // ปรับ path ตามจริง
 
+
 export default function App() {
   return (
     <NavigationContainer>
