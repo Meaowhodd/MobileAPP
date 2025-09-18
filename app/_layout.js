45f0a5704af8642cdcd4829c6d0f7f6f5e40912e
Starlipp04 <cokerter159630@gmail.com>
2025-09-18 22:49:36 +0700

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,11 @@
-// app/_layout.js
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {/* กลุ่มแท็บทั้งหมด */}
-      <Stack.Screen name="(tabs)" />
-      {/* หน้าที่อยู่นอกแท็บ (จะไม่เห็น Tab Bar) */}
-      <Stack.Screen name="screens/Booking" />
-      <Stack.Screen name="screens/Login" />
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="screens/Login" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
